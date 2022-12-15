f823314e0df79056c77e35a25e11b8b7a806dff0
duende14 <84480089+duende14@users.noreply.github.com>
2022-12-15 12:45:41 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from './Clock';
+import Clockf from './ClockF';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/*<Clock></Clock>*/}
+        <Clockf></Clockf>
+      </header>
+    </div>
+  );
+}
+
+export default App;
